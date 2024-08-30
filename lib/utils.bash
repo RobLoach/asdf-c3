17bceef7293595e9777aec0e903b04262f5809c1
Rob Loach <robloach@gmail.com>
2024-08-30 00:09:56 -0400

    Fix version

--- lib/utils.bash
+++ lib/utils.bash
@@ -46,7 +46,7 @@ download_release() {
 	esac
 
 	# TODO: Adapt the platform release URL convention for c3
-	url="$GH_REPO/releases/download/v${version}/c3-${platform}.tar.gz"
+	url="$GH_REPO/releases/download/${version}/c3-${platform}.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
