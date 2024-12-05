ffb4ab12b62ee10fcd6db81b0acf8964ceada763
Rob Loach <robloach@gmail.com>
2024-12-05 01:02:23 -0500

    Fix chmod

--- lib/utils.bash
+++ lib/utils.bash
@@ -76,6 +76,7 @@ install_version() {
 		# TODO: Assert c3c executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+		chmod +x "$install_path/$tool_cmd"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
